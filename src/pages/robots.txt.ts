144892dfa58e741ef91e457233980eb644a167c2
jeffheartmp <62608985+jeffheartmp@users.noreply.github.com>
2024-11-15 15:07:29 -0300

    updated layouts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,34 +1,15 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async () => {
-  const siteUrl = import.meta.env.PUBLIC_SITE_URL;
-  
-  const robotsTxt = `
-# www.robotstxt.org
-
+const robotsTxt = `
 User-agent: *
 Allow: /
-
-# Sitemaps
-Sitemap: ${siteUrl}/sitemap.xml
-
-# Crawl-delay
-Crawl-delay: 10
-
-# Directories
 Disallow: /api/
 Disallow: /admin/
-Disallow: /private/
 
-# Files
-Disallow: /*.json$
-Disallow: /*.xml$
-Disallow: /*.txt$
-
-# Query strings
-Disallow: /*?*
+Sitemap: ${import.meta.env.SITE_URL}/sitemap.xml
 `.trim();
 
+export const GET: APIRoute = () => {
   return new Response(robotsTxt, {
     headers: {
       'Content-Type': 'text/plain',
