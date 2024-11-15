e98aba0dbb0b667e768c7d67af6ff4c32668b865
jeffheartmp <62608985+jeffheartmp@users.noreply.github.com>
2024-11-15 16:57:12 -0300

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://tronoku.github.io',
-  base: '/thesearchprocom',
+  site: 'https://thesearchpro.com',
+  base: '',
   integrations: [tailwind()],
-  output: 'static',
+  server: {
+    headers: {
+      "X-Frame-Options": "SAMEORIGIN",
+      "X-Content-Type-Options": "nosniff",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "Permissions-Policy": "camera=(), microphone=(), geolocation=()",
+      "X-XSS-Protection": "1; mode=block",
+      "Content-Security-Policy": `
+        default-src 'self';
+        img-src 'self' data: https: http:;
+        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+        font-src 'self' https://fonts.gstatic.com;
+        script-src 'self' 'unsafe-inline' 'unsafe-eval';
+      `.replace(/\s+/g, ' ').trim()
+    }
+  }
 });
