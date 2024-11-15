0ebe893fa856479632a3727216e684a311e3b7df
jeffheartmp <62608985+jeffheartmp@users.noreply.github.com>
2024-11-15 19:37:16 -0300

    updated security policy

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -6,9 +6,6 @@ Allow: /
 Disallow: /api/
 Disallow: /admin/
 
-# Allow Google Tag Manager
-Allow: /gtm.js
-
 Sitemap: https://thesearchpro.com/sitemap.xml
 `.trim();
 
@@ -16,12 +13,6 @@ export const GET: APIRoute = () => {
   return new Response(robotsTxt, {
     headers: {
       'Content-Type': 'text/plain',
-      'Content-Security-Policy': `
-        default-src 'self';
-        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/maps https://www.googletagmanager.com;
-        frame-ancestors 'self';
-      `.trim().replace(/\s+/g, ' '),
-      'X-Frame-Options': 'SAMEORIGIN',
     },
   });
 }; 
