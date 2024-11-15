3dbed15800ead91f38766e9f155bd86e79522951
jeffheartmp <62608985+jeffheartmp@users.noreply.github.com>
2024-11-15 18:31:56 -0300

    updated links

--- src/utils/breadcrumbs.ts
+++ src/utils/breadcrumbs.ts
@@ -5,43 +5,43 @@ type BreadcrumbItem = {
 
 export const serviceBreadcrumbs = {
   seo: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Search Engine Optimization' }
   ],
   ppc: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Pay Per Click Marketing' }
   ],
   webDesign: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Web Design & Development' }
   ],
   socialMedia: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Social Media Marketing' }
   ],
   webAnalytics: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Web Analytics' }
   ],
   brandMarketing: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Brand Marketing' }
   ],
   localSearch: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Local Search Strategy' }
   ],
   mapsOptimization: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Maps Search Optimization' }
   ],
   linkBuilding: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Link Building & PR' }
   ],
   emailDesign: [
-    { text: 'Services', href: '/services' },
+    { text: 'Services', href: '/services/' },
     { text: 'Email Design' }
   ]
 } as const;
